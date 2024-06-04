444b00b906caa9fe10be32eb52bec6efc2327270
Olena Reukova <kronverk01@gmail.com>
2024-06-04 10:51:07 +0100

    Using Ternary Operators

--- client/src/components/ConditionalRendering.jsx
+++ client/src/components/ConditionalRendering.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Welcome from './Welcome';
+import Code from './Code';
 
 function ConditionalRendering() {
-  let message;
+  //ConditionalRendering - this is a little bit messi code
+  //   let message;
+  //   let display = true;
+  //   if (display) {
+  //     message = <h1>Message 1</h1>;
+  //   } else {
+  //     message = <h1>Message 2</h1>;
+  //   }
+  //   return <div>{message}</div>;
+
+  //good practice to use a ternary operator instead
   let display = true;
-  if (display) {
-    message = <h1>Message 1</h1>;
-  } else {
-    message = <h1>Message 2</h1>;
-  }
-  return <div>{message}</div>;
+  //   return display ? <h1>Message 1 </h1> : <h1>Message 2</h1>;
+  return display ? <Welcome /> : <Code />;
 }
 
 export default ConditionalRendering;
